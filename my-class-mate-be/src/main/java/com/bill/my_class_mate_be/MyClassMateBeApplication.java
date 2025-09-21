e435db2f86a8bdd06a9edf85fd445e76a226fb8b
Pongthanat Soontornpornlert <bccbew164@gmail.com>
2025-09-21 12:55:51 +0700

    BE init SpringBoot

---
+++ my-class-mate-be/src/main/java/com/bill/my_class_mate_be/MyClassMateBeApplication.java
@@ -0,0 +1,13 @@
+package com.bill.my_class_mate_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyClassMateBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyClassMateBeApplication.class, args);
+	}
+
+}
---
+++ my-class-mate-be/src/test/java/com/bill/my_class_mate_be/MyClassMateBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bill.my_class_mate_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyClassMateBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
