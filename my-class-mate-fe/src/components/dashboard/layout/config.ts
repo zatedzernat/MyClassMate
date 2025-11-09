bee83c96210938e6320161e46143faf3d9c74436
Pongthanat Soontornpornlert <bccbew164@gmail.com>
2025-11-09 14:44:48 +0700

    add check-in for staff and lec

--- my-class-mate-fe/src/components/dashboard/layout/config.ts
+++ my-class-mate-fe/src/components/dashboard/layout/config.ts
@@ -2,8 +2,8 @@ import type { NavItemConfig } from '@/types/nav';
 import { paths } from '@/paths';
 
 export const navItems = [
-  // { key: 'users', title: 'ข้อมูลผู้ใช้งาน', href: paths.dashboard.user, icon: 'users' },
   { key: 'course', title: 'ข้อมูลรายวิชา', href: paths.dashboard.course, icon: 'course' },
+  { key: 'today-course', title: 'เช็คชื่อเข้าเรียน', href: paths.dashboard.todayCourses, icon: 'todayCourse' },
   { key: 'participation', title: 'การมีส่วนร่วม', href: paths.dashboard.participation, icon: 'calendarStarIcon' },
 ] satisfies NavItemConfig[];
 
